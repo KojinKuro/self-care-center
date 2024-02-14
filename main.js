99d696104713e49e189df954f6d1cae86f634c60
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-13 20:43:55 -0800

    Change background and fade in text

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+let html = document.querySelector('html');
 let messageBox = document.querySelector('#message-random');
 let messageInput = document.querySelector('button[type="submit"]');
-let affirmationRadio = document.querySelector('input[type="radio"]');
+let messageForm = document.querySelector('#message-form');
+let affirmationRadio = document.querySelector('input#affirmation[type="radio"]');
+let mantraRadio = document.querySelector('input#mantra[type="radio"]');
 
+messageForm.addEventListener('change', changeBackground);
 messageInput.addEventListener('click', function(event) {
   event.preventDefault();
   var message = (affirmationRadio.checked) ? randomAffirmation() : randomMantra();
   setMessage(message);
 });
 
+function changeBackground() {
+  if (affirmationRadio.checked) { html.classList.remove('background-alt'); }
+  else { html.classList.add('background-alt') };
+}
+
 function setMessage(text) {
-  messageBox.innerText = text;
+  messageBox.innerHTML = `<span class='fade-up'>${text}</span>`
   messageBox.style.fontSize = '1.75rem';
 }
 
